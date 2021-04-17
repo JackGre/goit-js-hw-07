a5a22304e9e91f858e64e48db70c76a200e79f06
JackGre <gl.meh.bmes.od@gmail.com>
2021-04-17 19:05:14 +0300

    прописал функцию в 1 задачи

--- js/task-01.js
+++ js/task-01.js
@@ -5,9 +5,9 @@ console.log(`'В списке ${itemLinkEl} категории.'`);*/
 const ulCategoriesEl = document.querySelector('#categories');
 const itemLinkEl = ulCategoriesEl.children;
 
-const ggg = elem => { return itemLinkEl.length;    
+const counterLink = element => { return itemLinkEl.length;    
 };
-console.log(`'В списке ${ggg(itemLinkEl)} категории.'`)
-ggg()
+console.log(`'В списке ${counterLink(itemLinkEl)} категории.'`)
+counterLink();
 
 
